f46ca3c0d6d8d002e6a1c8924ec1c4f3259e0681
Mason Stenquist <mstenquist@developertown.com>
2021-09-02 08:51:58 -0400

    added amplify

--- src/pages/PostNew.tsx
+++ src/pages/PostNew.tsx
@@ -1,18 +1,35 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router";
+import { CreatePostMutation, CreatePostMutationVariables, PostStatus } from "../API";
+import ampAPI from "../api/ampAPI";
 import FileUpload, { Files } from "../components/inputs/FileUpload";
 import Input from "../components/inputs/Input";
 import TextArea from "../components/inputs/TextArea";
+import { createPost } from "../graphql/mutations";
+import { routes } from "../ui/routes";
+
 const PostNew: React.FC = () => {
+  const history = useHistory();
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [files, setFiles] = useState<Files>({});
+  const submit = async () => {
+    const post = await ampAPI<CreatePostMutation, CreatePostMutationVariables>(createPost, {
+      input: { title, body, status: PostStatus.PUBLISHED },
+    });
 
+    history.push(routes.root.show.with({ id: post.createPost?.id }));
+    console.log(post);
+  };
   return (
     <div>
       <h1 className="font-thin">New Post</h1>
       <Input className="py-4" label="Title" value={title} onChange={setTitle} />
       <TextArea className="py-4" label="Body" rows={3} value={body} onChange={setBody} />
       <FileUpload multiple label="Photos" value={files} onChange={(files) => setFiles(files)} />
+      <button onClick={submit} className="btn-primary">
+        Submit
+      </button>
     </div>
   );
 };
