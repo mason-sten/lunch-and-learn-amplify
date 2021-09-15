10d9cb384f2df081c1f36167551f803572531e04
Mason Stenquist <mstenquist@developertown.com>
2021-09-15 12:17:32 -0400

    added amp

---
+++ src/graphql/mutations.ts
@@ -0,0 +1,154 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createPost = /* GraphQL */ `
+  mutation CreatePost(
+    $input: CreatePostInput!
+    $condition: ModelPostConditionInput
+  ) {
+    createPost(input: $input, condition: $condition) {
+      id
+      title
+      body
+      status
+      images {
+        items {
+          id
+          postID
+          key
+          createdAt
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updatePost = /* GraphQL */ `
+  mutation UpdatePost(
+    $input: UpdatePostInput!
+    $condition: ModelPostConditionInput
+  ) {
+    updatePost(input: $input, condition: $condition) {
+      id
+      title
+      body
+      status
+      images {
+        items {
+          id
+          postID
+          key
+          createdAt
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deletePost = /* GraphQL */ `
+  mutation DeletePost(
+    $input: DeletePostInput!
+    $condition: ModelPostConditionInput
+  ) {
+    deletePost(input: $input, condition: $condition) {
+      id
+      title
+      body
+      status
+      images {
+        items {
+          id
+          postID
+          key
+          createdAt
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const createImage = /* GraphQL */ `
+  mutation CreateImage(
+    $input: CreateImageInput!
+    $condition: ModelImageConditionInput
+  ) {
+    createImage(input: $input, condition: $condition) {
+      id
+      postID
+      post {
+        id
+        title
+        body
+        status
+        images {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      key
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateImage = /* GraphQL */ `
+  mutation UpdateImage(
+    $input: UpdateImageInput!
+    $condition: ModelImageConditionInput
+  ) {
+    updateImage(input: $input, condition: $condition) {
+      id
+      postID
+      post {
+        id
+        title
+        body
+        status
+        images {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      key
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteImage = /* GraphQL */ `
+  mutation DeleteImage(
+    $input: DeleteImageInput!
+    $condition: ModelImageConditionInput
+  ) {
+    deleteImage(input: $input, condition: $condition) {
+      id
+      postID
+      post {
+        id
+        title
+        body
+        status
+        images {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      key
+      createdAt
+      updatedAt
+    }
+  }
+`;
---
+++ src/graphql/queries.ts
@@ -0,0 +1,96 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getPost = /* GraphQL */ `
+  query GetPost($id: ID!) {
+    getPost(id: $id) {
+      id
+      title
+      body
+      status
+      images {
+        items {
+          id
+          postID
+          key
+          createdAt
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listPosts = /* GraphQL */ `
+  query ListPosts(
+    $filter: ModelPostFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listPosts(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        title
+        body
+        status
+        images {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
+export const getImage = /* GraphQL */ `
+  query GetImage($id: ID!) {
+    getImage(id: $id) {
+      id
+      postID
+      post {
+        id
+        title
+        body
+        status
+        images {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      key
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listImages = /* GraphQL */ `
+  query ListImages(
+    $filter: ModelImageFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listImages(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        postID
+        post {
+          id
+          title
+          body
+          status
+          createdAt
+          updatedAt
+        }
+        key
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
